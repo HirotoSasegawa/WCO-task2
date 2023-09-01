0af4e48ec8237cea27be8a5605a0ef6168f4afab
HirotoSasegawa <139758728+HirotoSasegawa@users.noreply.github.com>
2023-09-01 14:28:23 +0000

    [Update] update失敗時のエラーメッセージ実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   post 'books' => 'books#create'
   get 'books' => 'books#index'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   resources :books
 end
